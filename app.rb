dd16fc274f9d869478a042245838b07327660bd0
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-02-01 12:58:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,13 +16,9 @@ class App < Sinatra::Base
     @number = params[:number].to_i
     @phrase = params[:phrase]
     @total = []
-    @counter = 0
-    until @counter = @number
-      @counter += 1
+    @number.times do
       @total << @phrase
     end
-    binding.pry
-    "#{@total.join(" ")}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
