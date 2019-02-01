5a9a22d5f3a6344c03a52c23369758b936003376
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-02-01 12:59:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
     @number.times do
       @total << @phrase
     end
-    binding.pry
+    "#{@total.join}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
