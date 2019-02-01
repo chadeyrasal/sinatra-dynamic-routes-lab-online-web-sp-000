e159c5b4eb0c4daef2bf361235cf8490191345b4
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-02-01 12:45:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,7 +31,7 @@ class App < Sinatra::Base
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     if @operation == "add"
-
+      "#{@number1 + @number2}"
     elsif @operation == "substract"
     elsif @operation == "multiply"
     elsif @operation == "divide"
