728a233c04de184ec06d0a24c40eb9df88afc508
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-02-01 12:46:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,8 +33,11 @@ class App < Sinatra::Base
     if @operation == "add"
       "#{@number1 + @number2}"
     elsif @operation == "substract"
+      "#{@number1 - @number2}"
     elsif @operation == "multiply"
+      "#{@number1 * @number2}"
     elsif @operation == "divide"
+      "#{@number1 / @number2}"
     end
   end
 
