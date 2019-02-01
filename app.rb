b87060f7c4c3b5ba9ef61b4277ec064204c63ec7
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-02-01 12:32:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase].gsub("%20", " ")
+    binding.pry
     @number.times do
       "#{@phrase}"
     end
